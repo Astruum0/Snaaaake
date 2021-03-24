563c24f73584f613dbe37046ac1dca0e855b81b5
Mattéo <60100878+Matteo-Ynov@users.noreply.github.com>
2021-03-24 15:36:58 +0100

    added name validation

--- frontend/index.js
+++ frontend/index.js
@@ -16,12 +16,21 @@ function clear() {
     }
 }
 
-function refresh() {
-    clear();
-    container = document.getElementById("container");
+function clear() {
+    for (i = 0; i <= Math.ceil(serversStates.length / 4); i++) {
+        var card_deck = document.getElementById("server" + i);
+        if (typeof card_deck != "undefined" && card_deck != null) {
+            card_deck.remove();
+        }
+    }
+}
 
+function refresh() {
+    clear()
+    container = document.getElementById("container");
     for (i = 0; i < serversStates.length; i++) {
         if (!serversStates[i].private) {
+            var id = serversStates[i].id
             var card_deck = document.createElement("div");
             card_deck.classList.add("card-deck");
             card_deck.id = "server" + i;
@@ -61,13 +70,26 @@ function refresh() {
             status.classList.add("card-text");
             status.innerHTML = "Status : " + game_status;
 
-            var form = document.createElement("a");
-            form.setAttribute(
-                "href",
-                "game.html?id=" + serversStates[i].id + "&username=" + "Matteo"
-            );
-
+            var username = document.getElementById("username").value
             var button = document.createElement("button");
+
+            if(username != ""){
+                button.addEventListener("click", function() {
+                    window.location.href = "game.html?id=" + id + "&username=" + username
+                  });
+            } else {
+                button.addEventListener("click", function() {
+                    error_div = document.createElement("div")
+                    error_div.innerText = "Veuillez choisir un pseudo"
+                    error_div.classList.add("alert")
+                    error_div.classList.add("alert-danger")
+                    error_div.classList.add("login-err")
+                    $(error_div).hide().appendTo(document.getElementById("error_div")).fadeIn(500);
+                    setTimeout(function() {
+                        $(error_div).fadeOut(500, function() { $(this).remove() })
+                    }, 3000)
+                });
+            }
             button.classList.add(
                 "btn",
                 game_button_class,
@@ -83,8 +105,7 @@ function refresh() {
             card.appendChild(body);
             body.appendChild(title);
             body.appendChild(status);
-            status.appendChild(form);
-            form.appendChild(button);
+            body.appendChild(button)
         }
     }
 }
