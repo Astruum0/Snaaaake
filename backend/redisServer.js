0579b4703f7d824115b45d0da21676b48ab04d92
arthur.vella@ynov.com <60743111+Astruum0@users.noreply.github.com>
2021-03-25 15:00:03 +0100

    Add docker exec

--- backend/redisServer.js
+++ backend/redisServer.js
@@ -19,5 +19,30 @@ io.on("connection", (client) => {
             client.emit("getPortFromID", server.port);
         });
     });
+    client.on("createNewServer", (private) => {
+        getAllServers().then((servers) => {
+            usedPorts = [];
+            for (var i = 0; i < servers.length; i++) {
+                usedPorts.push(servers[i].port);
+            }
+            for (var currentPort = 3001; currentPort < 3100; currentPort++) {
+                if (!usedPorts.includes(currentPort)) {
+                    var serverId = createNewServer(currentPort, private);
+                    //Creation Docker
+                    const { exec } = require("child_process");
+
+                    exec(
+                        "docker run --name " +
+                        serverId +
+                        " -dp " +
+                        currentPort +
+                        ":3000 snakefinal"
+                    );
+
+                    break;
+                }
+            }
+        });
+    });
 });
 io.listen(3000);
